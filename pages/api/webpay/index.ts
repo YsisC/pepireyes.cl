39c111998dea090a8c80a985099140880b9059c1
YsisC <ysisclongart@gmail.com>
2023-11-06 18:00:38 -0300

    updated router url

--- pages/api/webpay/index.ts
+++ pages/api/webpay/index.ts
@@ -1,54 +1,53 @@
-import {  WebPayOrderConfirmResponse } from '@/interfaces/webpay';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { WebPayOrderConfirmResponse } from "@/interfaces/webpay";
+import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 type Data = {
-    message: string
-    | WebPayOrderConfirmResponse
-}
+  message: string | WebPayOrderConfirmResponse;
+};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    switch (req.method) {
-        case "PUT":
-          return webpay_respuesta(req, res);
-    
-        default:
-          return res.status(400).json({ message: "Bad request" });
-      }
-   
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "PUT":
+      return webpay_respuesta(req, res);
+
+    default:
+      return res.status(400).json({ message: "Bad request" });
+  }
 }
 const headers = {
   "Content-Type": "application/json",
   "Tbk-Api-Key-Id": process.env.WEBPAY_ID,
   "Tbk-Api-Key-Secret": process.env.WEBPAY_SECRET,
 };
-const webpay_respuesta = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
- 
+const webpay_respuesta = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
   const { token_ws } = req.query;
-  console.log(token_ws)
-  const urlToken = process.env.WEBPAY_URL + "/" + token_ws
-  console.log("toma el url token", urlToken)
+  console.log(token_ws);
+
   const data = {};
   try {
-  const response = await axios.put(
-    process.env.WEBPAY_URL + "/" + token_ws,
-    data,
-    {
-      headers: headers,
+    if (typeof token_ws === "string") {
+      const urlToken = process.env.WEBPAY_URL + "/" + token_ws;
+      console.log("toma el url token", urlToken);
+      const response = await axios.put(urlToken, data, {
+        headers: headers,
+      });
+      console.log("data api put", response.data);
+      return res.status(200).json(response.data);
     }
-  );
-
-  console.log("data api put", response.data);
-     // Send a successful response with the data from the external API
-     return res.status(200).json(response.data);
-} catch (error) {
-  if (axios.isAxiosError(error)) {
-    console.log(error.response?.data);
-    return res.status(500).json({ message: "Internal Server Error" });
-  } else {
-    console.log(error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    // Send a successful response with the data from the external API
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+      return res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
   }
-}
-
-
-}
+};
--- pages/webpay/pay/index.tsx
+++ pages/webpay/pay/index.tsx
@@ -3,7 +3,9 @@ import { ShopLayout } from "@/components/layouts";
 import { useRouter } from 'next/router';
 const WebpayPayPage = () => {
   const router = useRouter();
-  const url =  router.query.url;
+  const url = Array.isArray(router.query.url)
+  ? router.query.url[0] // Si es una matriz, toma el primer elemento
+  : router.query.url; // Si es una cadena, déjalo igual
   const token =  router.query.token;
   // Comprobación antes de acceder a token.length
 
@@ -11,10 +13,12 @@ console.log("url", {url})
 console.log("token", {token})
 useEffect(() => {
     
-  const formElement = document.getElementById("myForm");
-  if (formElement ) {
-    formElement.submit();
+  const formElement = document.getElementById("myForm") as HTMLFormElement | null;
+  if (formElement) {
+    formElement.submit(); // Utiliza el método submit() en un elemento de formulario
   }
+  
+  
 }, []);
 
   return (
@@ -22,7 +26,7 @@ useEffect(() => {
       <section className="paddings">
         <h1>Webpay de Transbank</h1>
         Pagando.....
-        <form id="myForm" action={url} name="form" method="POST">
+        <form id="myForm" action={url || ""} name="form" method="POST">
           <input
             type="hidden"
             name="token_ws"
